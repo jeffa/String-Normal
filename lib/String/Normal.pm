b5f3984a1280311e03c91100439458fde7b18ea3
jeffa <jeffa@unlocalhost.com>
2016-06-24 14:26:04 -0400

    refactored relevant code into Name type

--- lib/String/Normal.pm
+++ lib/String/Normal.pm
@@ -5,159 +5,29 @@ our $VERSION = '0.01';
 
 use String::Normal::Type::Name;
 
-use Lingua::Stem;
-our $STEM;
-our %name_stop;
-our %name_compress;
-
 sub new {
     my $self = shift;
-    $STEM = Lingua::Stem->new;
-    $STEM->add_exceptions( String::Normal::Type::Name->stem ); 
-    %name_stop     = %{ String::Normal::Type::Name->stop };
-    %name_compress = %{ String::Normal::Type::Name->compress };
     return bless {@_}, $self;
 }
 
 # currently only handles name types
 sub transform {
-    my ($self,$value) = @_;
+    my ($self,$value,$type) = @_;
 
-    $value = lc( $value );
-
-    $value =~ tr/  //s;          # squeeze multiple spaces to one
-    $value =~ s/^ //;            # trim leading space
-    $value =~ s/ $//;            # trim trailing space
+    $value = lc( $value );      # lowercase
+    $value =~ tr/  //s;         # squeeze multiple spaces to one
+    $value =~ s/^ //;           # trim leading space
+    $value =~ s/ $//;           # trim trailing space
 
     # strip out control chars except tabs, lf's, cr's, r single quote, mdash and ndash
     $value =~ s/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F-\x91\x93-\x95\x98-\x9F]//g;
 
-    # tokenize and stem
-    my (@digits,@words);
-    _tokenize_name( $value, \@digits, \@words );
-    $STEM->stem_in_place( @words );
-
-    # Remove "special" beginning and/or ending stopwords, if such words are present
-    # and enough tokens are in place to remove them safely.
-    if (@words) {
-        # make a copy of @words and whittle it down
-        my @copy = @words;
-        my $count;
-        if ($count = $name_stop{first}->{$copy[0]}) {
-            shift @copy if @copy >= $count;
-        }
-        if (@copy and $count = $name_stop{last}->{$copy[-1]}) {
-            pop @copy if @copy >= $count;
-        }
-
-        # reverting back if overnormalization occurs
-        @words = @copy if @copy;
+    if (!$type or $type eq 'name') {
+        my $name = String::Normal::Type::Name->new;
+        return $name->transform( $value );
     }
 
-    # Remove all middle stop words that are safe to remove, based on the number of
-    # tokens, of course.
-    my @filtered = map {
-        my $count = $name_stop{middle}->{$_} || '';
-        (length $count and @words >= $count) ? () : $_;
-    } @words;
-
-    # If we filtered all words out, "revert" to the full array of stemmed tokens.
-    @filtered = @words unless @filtered;
-
-    # The canon name is the sorted filtered stemmed words plus the original digits.
-    $value = join ' ', sort @digits, @filtered;
-
-    # Mark this record as an error if we removed every single token.  Should not happen.
-#    unless (length $value) {
-#        return $value, @{ $errors{EMPTY_NAME} };
-#    }
-
-    return $value;
-}
-
-sub _tokenize_name {
-    my ($value,$digits,$words) = @_;
-
-    $value = _scrub_value( $value );
-
-    # split tokens on more than just whitespace:
-    # split digits from words but keep things like 3D and 1st combined,
-    # also split things like abcd#efgh but keep pound signs for #2 and # 1 and #
-    # prevent the empty string from finding its way into the token list as well
-    my @tokens = map { map length $_ ? $_ : (), split /##+|\s+|#+\b|\b#+/, $_ } $value =~ /(?:\d+\w{1,2}\b|\d+|\D+)/g;
-
-    # walk each token thru the tree and create markers
-    my @pairs = _mark_pairs( \@tokens );
-    _compress_list( \@tokens, \@pairs ) if @pairs;
-
-    # separate out tokens that contain digits (snowball stemmer will scrub all digits)
-    for (@tokens) {
-        if (/\d/) {
-            push @$digits, $_;
-        } else {
-            push @$words, $_;
-        }
-    }
-}
-
-sub _scrub_value {
-    my $value = shift;
-
-    $value = _deaccent_value( $value );
-    $value =~ tr/'//d;
-
-    # replace all rejected charactes with space
-    $value =~ s/[^a-z0-9#]/ /g;
-
-    return $value
-}
-
-sub _deaccent_value {
-    my $value = shift;
-
-    # remove decorations and stem variations of single quotes
-    $value =~ tr[àáâãäåæçèéêëìíîïñòóôõöøùúûüýÿ’`\x92]
-                [aaaaaaaceeeeiiiinoooooouuuuyy'''];
-
-    return $value;
-}
-
-
-sub _mark_pairs {
-    my $tokens = shift;
-    my @pairs = ();
-    for my $i (0 .. $#$tokens) {
-        my $token = $tokens->[$i];
-        next unless exists $name_compress{$token};
-        next if $i + 1 > $#$tokens;
-        my $end = _walk_tree( $i + 1, $tokens, $name_compress{$token} );
-        if ($end) {
-            push @pairs, [$i,$end];
-            $i = $end;
-        }
-    }
-    return @pairs;
-}
-
-
-sub _walk_tree {
-    my ($i, $list, $tree) = @_;
-
-    if (my $t = $tree->{$list->[$i]}) {
-        if (ref $t eq 'HASH' and !%$t) {
-            return $i;
-        } else {
-            _walk_tree( $i + 1, $list, $t );
-        }
-    }
-}
-
-sub _compress_list {
-    my ($list,$pairs) = @_;
-    for my $pair (reverse @$pairs) {
-        my ($s,$e) = @$pair;
-        splice @$list, $s, $e - $s + 1, join '', @$list[$s .. $e];
-    }
+    return "type $type is not implemented";
 }
 
 1;
--- lib/String/Normal/Type/Name.pm
+++ lib/String/Normal/Type/Name.pm
@@ -3,6 +3,20 @@ use strict;
 use warnings;
 use String::Normal::Type;
 
+use Lingua::Stem;
+our $STEM;
+our $name_stop;
+our $name_compress;
+
+sub new {
+    my $self = shift;
+    $STEM = Lingua::Stem->new;
+    $STEM->add_exceptions( $self->stem );  #TODO: override default files
+    $name_stop     = $self->stop;
+    $name_compress = $self->compress;
+    return bless {@_}, $self;
+}
+
 sub stem {
     my ($self,$file) = @_;
     my %stem = String::Normal::Type::_slurp_file( $file || 'name_stem.txt' );
@@ -42,6 +56,138 @@ sub compress {
     return \%compress;
 }
 
+
+sub transform {
+    my ($self,$value) = @_;
+
+    # tokenize and stem
+    my (@digits,@words);
+    _tokenize_name( $value, \@digits, \@words );
+    $STEM->stem_in_place( @words );
+
+    # Remove "special" beginning and/or ending stopwords, if such words are present
+    # and enough tokens are in place to remove them safely.
+    if (@words) {
+        # make a copy of @words and whittle it down
+        my @copy = @words;
+        my $count;
+        if ($count = $name_stop->{first}{$copy[0]}) {
+            shift @copy if @copy >= $count;
+        }
+        if (@copy and $count = $name_stop->{last}{$copy[-1]}) {
+            pop @copy if @copy >= $count;
+        }
+
+        # reverting back if overnormalization occurs
+        @words = @copy if @copy;
+    }
+
+    # Remove all middle stop words that are safe to remove, based on the number of
+    # tokens, of course.
+    my @filtered = map {
+        my $count = $name_stop->{middle}{$_} || '';
+        (length $count and @words >= $count) ? () : $_;
+    } @words;
+
+    # If we filtered all words out, "revert" to the full array of stemmed tokens.
+    @filtered = @words unless @filtered;
+
+    # The canon name is the sorted filtered stemmed words plus the original digits.
+    $value = join ' ', sort @digits, @filtered;
+
+    # Mark this record as an error if we removed every single token.  Should not happen.
+#    unless (length $value) {
+#        return $value, @{ $errors{EMPTY_NAME} };
+#    }
+
+    return $value;
+}
+
+sub _tokenize_name {
+    my ($value,$digits,$words) = @_;
+
+    $value = _scrub_value( $value );
+
+    # split tokens on more than just whitespace:
+    # split digits from words but keep things like 3D and 1st combined,
+    # also split things like abcd#efgh but keep pound signs for #2 and # 1 and #
+    # prevent the empty string from finding its way into the token list as well
+    my @tokens = map { map length $_ ? $_ : (), split /##+|\s+|#+\b|\b#+/, $_ } $value =~ /(?:\d+\w{1,2}\b|\d+|\D+)/g;
+
+    # walk each token thru the tree and create markers
+    my @pairs = _mark_pairs( \@tokens );
+    _compress_list( \@tokens, \@pairs ) if @pairs;
+
+    # separate out tokens that contain digits (snowball stemmer will scrub all digits)
+    for (@tokens) {
+        if (/\d/) {
+            push @$digits, $_;
+        } else {
+            push @$words, $_;
+        }
+    }
+}
+
+sub _scrub_value {
+    my $value = shift;
+
+    $value = _deaccent_value( $value );
+    $value =~ tr/'//d;
+
+    # replace all rejected charactes with space
+    $value =~ s/[^a-z0-9#]/ /g;
+
+    return $value
+}
+
+sub _deaccent_value {
+    my $value = shift;
+
+    # remove decorations and stem variations of single quotes
+    $value =~ tr[àáâãäåæçèéêëìíîïñòóôõöøùúûüýÿ’`\x92]
+                [aaaaaaaceeeeiiiinoooooouuuuyy'''];
+
+    return $value;
+}
+
+
+sub _mark_pairs {
+    my $tokens = shift;
+    my @pairs = ();
+    for my $i (0 .. $#$tokens) {
+        my $token = $tokens->[$i];
+        next unless exists $name_compress->{$token};
+        next if $i + 1 > $#$tokens;
+        my $end = _walk_tree( $i + 1, $tokens, $name_compress->{$token} );
+        if ($end) {
+            push @pairs, [$i,$end];
+            $i = $end;
+        }
+    }
+    return @pairs;
+}
+
+
+sub _walk_tree {
+    my ($i, $list, $tree) = @_;
+
+    if (my $t = $tree->{$list->[$i]}) {
+        if (ref $t eq 'HASH' and !%$t) {
+            return $i;
+        } else {
+            _walk_tree( $i + 1, $list, $t );
+        }
+    }
+}
+
+sub _compress_list {
+    my ($list,$pairs) = @_;
+    for my $pair (reverse @$pairs) {
+        my ($s,$e) = @$pair;
+        splice @$list, $s, $e - $s + 1, join '', @$list[$s .. $e];
+    }
+}
+
 1;
 
 __END__
@@ -56,6 +202,18 @@ This package defines substitutions to be performed on the name types.
 =head1 METHODS
 
 =over 4
+
+=item C<new( %params )>
+
+    my $name = String::Normal::Type::Name->new;
+
+Creates a Name type.
+
+=item C<transform( $value )>
+
+    my $new_value = $name->transform( $value );
+
+Transforms a value according to the rules of a Name type.
 
 =item C<stem( %params )>
 
