b0d93de029fc9fc3f778b73dec93cbc1d3d2c07c
jeffa <jeffa@unlocalhost.com>
2016-07-01 14:33:15 -0400

    removed start and stop from title/add stem; more docs

--- lib/String/Normal/Config/AddressStop.pm
+++ lib/String/Normal/Config/AddressStop.pm
@@ -19,20 +19,14 @@ sub _data {
         next if /^#/;
         my ($word,$count) = split ',', $_;
         $count ||= 1;
-
-        if (substr( $word, 0, 1 ) eq '^') {
-            substr( $word, 0, 1 ) = '';
-            $stop{first}->{$word} = $count;
-        } elsif (substr( $word, -1, 1 ) eq '$') {
-            substr( $word, -1, 1 ) = '';
-            $stop{last}->{$word} = $count;
-        } else {
-            $stop{middle}->{$word} = $count;
-        }
+        $stop{middle}->{$word} = $count;
     }
 
     return \%stop;
 }
+
+1;
+
 =head1 NAME
 
 String::Normal::Config::AddressStop;
@@ -41,9 +35,75 @@ String::Normal::Config::AddressStop;
 
 This package defines removals to be performed on address types.
 
+=head1 STRUCTURE
+
+One entry per line. Each value will be removed. Special characters
+C<^> and C<$> are NOT used. You can prepend C<#> to lines that you
+wish to skip. For example:
+
+    the
+    #and
+
+Would remove all occurances of C<the> but not C<and>. Special character
+C<,> is used to override removal if the final number of tokens if less
+then the digit specified. For example:
+
+    the,2
+
+Would remove all occurances of C<the> in "The Generic Title" but would
+not remove any occurances in "The The".
+
+You can provide your own data by creating a text file containing your
+values and provide the path to that file via the constructor:
+
+  my $normalizer = String::Normal->new( address_stop => '/path/to/values.txt' );
+
+=head1 AUTHOR
+
+Jeff Anderson, C<< <jeffa at cpan.org> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2016 Jeff Anderson.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 =cut
 
-1;
 __DATA__
 #ave
 #blvd
--- lib/String/Normal/Config/TitleStem.pm
+++ lib/String/Normal/Config/TitleStem.pm
@@ -18,6 +18,8 @@ sub _data {
     return \%stem;
 }
 
+1;
+
 =head1 NAME
 
 String::Normal::Config::TitleStem;
@@ -26,9 +28,66 @@ String::Normal::Config::TitleStem;
 
 This package defines substitutions to be performed on title types.
 
+=head1 STRUCTURE
+
+One entry pair per line: first the value to be matched then the value
+to be changed to. For example:
+
+  foo fu
+
+Would change all occurances of C<foo> to C<fu>. See C<__DATA__> block below.
+
+You can provide your own data by creating a text file containing your
+values and provide the path to that file via the constructor:
+
+  my $normalizer = String::Normal->new( title_stem => '/path/to/values.txt' );
+
+=head1 AUTHOR
+
+Jeff Anderson, C<< <jeffa at cpan.org> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2016 Jeff Anderson.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 =cut
 
-1;
 __DATA__
 ii 2
 iii 3
